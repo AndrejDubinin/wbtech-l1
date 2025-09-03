a50cb1445c37d43b468feb13f869d8038ec72413
AndrejDubinin <dubininandrej046@gmail.com>
2025-09-03 11:26:32 -0500

    [L1] add L1.8 task

--- l1_8/l1_8.go
+++ l1_8/l1_8.go
@@ -7,21 +7,19 @@ import (
 )
 
 func main() {
-	var x int
-
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <num_workers>")
 		return
 	}
 
-	x, err := strconv.Atoi(os.Args[1])
+	x, err := strconv.ParseInt(os.Args[1], 10, 64)
 	if err != nil {
 		fmt.Printf("parameter: %v\n", err)
 	}
 
-	fmt.Printf("number: %b\n", x)
+	fmt.Printf("number: %d, byte: %b\n", x, x)
 
 	x = x &^ 1
 
-	fmt.Printf("after clear bit: %b\n", x)
+	fmt.Printf("after clear bit: %d, byte: %b\n", x, x)
 }
