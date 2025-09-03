57c7843f8d315affa7c3f8ac4ee2ef868b3eebb9
AndrejDubinin <dubininandrej046@gmail.com>
2025-09-03 11:20:00 -0500

    [L1] add L1.8 task

---
+++ l1_8/l1_8.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var x int
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <num_workers>")
+		return
+	}
+
+	x, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("parameter: %v\n", err)
+	}
+
+	fmt.Printf("number: %b\n", x)
+
+	x = x &^ 1
+
+	fmt.Printf("after byte shift: %b\n", x)
+}
